6c5e976a589c72353bf3e67d0e6e2770953acfc1
Mehmet Yılmaz <yilmazmehmet7711@gmail.com>
2024-10-19 11:19:50 +0300

    Proje dosyası ekle.

---
+++ ToDoUygulamasi/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    // Kart büyüklükleri Enum
+    enum Buyukluk { XS = 1, S, M, L, XL }
+
+    // Kart sınıfı
+    class Kart
+    {
+        public string Baslik { get; set; }
+        public string Icerik { get; set; }
+        public string AtananKisi { get; set; }
+        public Buyukluk Buyukluk { get; set; }
+    }
+
+    // Board Line'larını tutan Dictionary yapısı
+    static Dictionary<string, List<Kart>> board = new Dictionary<string, List<Kart>>()
+    {
+        { "TODO", new List<Kart>() },
+        { "IN PROGRESS", new List<Kart>() },
+        { "DONE", new List<Kart>() }
+    };
+
+    // Takım üyeleri ID - İsim ilişkisi
+    static Dictionary<int, string> takimUyeleri = new Dictionary<int, string>()
+    {
+        { 1, "Ali" },
+        { 2, "Ayşe" },
+        { 3, "Veli" },
+        { 4, "Fatma" }
+    };
+
+    static void Main(string[] args)
+    {
+        // Varsayılan kartlar
+        board["TODO"].Add(new Kart { Baslik = "Proje Başlat", Icerik = "Araştırma yapılacak", AtananKisi = takimUyeleri[1], Buyukluk = Buyukluk.M });
+        board["IN PROGRESS"].Add(new Kart { Baslik = "UI Tasarımı", Icerik = "UI tamamlanacak", AtananKisi = takimUyeleri[2], Buyukluk = Buyukluk.L });
+        board["DONE"].Add(new Kart { Baslik = "Araştırma", Icerik = "Proje araştırması bitti", AtananKisi = takimUyeleri[3], Buyukluk = Buyukluk.S });
+
+        int secim;
+
+        do
+        {
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :) ");
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("(1) Board Listelemek");
+            Console.WriteLine("(2) Board'a Kart Eklemek");
+            Console.WriteLine("(3) Board'dan Kart Silmek");
+            Console.WriteLine("(4) Kart Taşımak");
+            Console.WriteLine("(0) Çıkış");
+
+            secim = int.Parse(Console.ReadLine());
+
+            switch (secim)
+            {
+                case 1:
+                    BoardListele();
+                    break;
+                case 2:
+                    KartEkle();
+                    break;
+                case 3:
+                    KartSil();
+                    break;
+                case 4:
+                    KartTasi();
+                    break;
+                case 0:
+                    Console.WriteLine("Çıkış yapılıyor...");
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz bir işlem!");
+                    break;
+            }
+
+        } while (secim != 0);
+    }
+
+    // Board'u Listeleme
+    static void BoardListele()
+    {
+        foreach (var kolon in board)
+        {
+            Console.WriteLine($"{kolon.Key} Line");
+            Console.WriteLine("************************");
+
+            if (kolon.Value.Count == 0)
+            {
+                Console.WriteLine("~ BOŞ ~");
+            }
+            else
+            {
+                foreach (var kart in kolon.Value)
+                {
+                    Console.WriteLine($"Başlık      : {kart.Baslik}");
+                    Console.WriteLine($"İçerik      : {kart.Icerik}");
+                    Console.WriteLine($"Atanan Kişi : {kart.AtananKisi}");
+                    Console.WriteLine($"Büyüklük    : {kart.Buyukluk}");
+                    Console.WriteLine("-");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    // Kart Ekleme
+    static void KartEkle()
+    {
+        Kart yeniKart = new Kart();
+
+        Console.Write("Başlık Giriniz                                  : ");
+        yeniKart.Baslik = Console.ReadLine();
+
+        Console.Write("İçerik Giriniz                                  : ");
+        yeniKart.Icerik = Console.ReadLine();
+
+        Console.Write("Büyüklük Seçiniz -> XS(1),S(2),M(3),L(4),XL(5)  : ");
+        yeniKart.Buyukluk = (Buyukluk)int.Parse(Console.ReadLine());
+
+        Console.Write("Kişi Seçiniz                                    : ");
+        foreach (var uye in takimUyeleri)
+        {
+            Console.WriteLine($"{uye.Key} - {uye.Value}");
+        }
+
+        int kisiID = int.Parse(Console.ReadLine());
+
+        if (takimUyeleri.ContainsKey(kisiID))
+        {
+            yeniKart.AtananKisi = takimUyeleri[kisiID];
+            board["TODO"].Add(yeniKart);
+            Console.WriteLine("Kart başarıyla eklendi!\n");
+        }
+        else
+        {
+            Console.WriteLine("Hatalı girişler yaptınız!\n");
+        }
+    }
+
+    // Kart Silme
+    static void KartSil()
+    {
+        Console.Write("Lütfen silmek istediğiniz kartın başlığını yazınız: ");
+        string baslik = Console.ReadLine();
+
+        foreach (var kolon in board)
+        {
+            var kart = kolon.Value.FirstOrDefault(k => k.Baslik == baslik);
+            if (kart != null)
+            {
+                kolon.Value.Remove(kart);
+                Console.WriteLine("Kart başarıyla silindi!\n");
+                return;
+            }
+        }
+
+        Console.WriteLine("Aradığınız kriterlere uygun kart board'da bulunamadı.\n");
+    }
+
+    // Kart Taşıma
+    static void KartTasi()
+    {
+        Console.Write("Lütfen taşımak istediğiniz kartın başlığını yazınız: ");
+        string baslik = Console.ReadLine();
+
+        foreach (var kolon in board)
+        {
+            var kart = kolon.Value.FirstOrDefault(k => k.Baslik == baslik);
+            if (kart != null)
+            {
+                Console.WriteLine($"Bulunan Kart Bilgileri:\nBaşlık: {kart.Baslik}\nİçerik: {kart.Icerik}\nAtanan Kişi: {kart.AtananKisi}\nBüyüklük: {kart.Buyukluk}");
+                Console.WriteLine("Lütfen taşımak istediğiniz Line'ı seçiniz: (1) TODO (2) IN PROGRESS (3) DONE");
+
+                int lineSecimi = int.Parse(Console.ReadLine());
+
+                string yeniLine = lineSecimi switch
+                {
+                    1 => "TODO",
+                    2 => "IN PROGRESS",
+                    3 => "DONE",
+                    _ => null
+                };
+
+                if (yeniLine != null)
+                {
+                    kolon.Value.Remove(kart);
+                    board[yeniLine].Add(kart);
+                    Console.WriteLine("Kart başarıyla taşındı!\n");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı bir seçim yaptınız!\n");
+                    return;
+                }
+            }
+        }
+
+        Console.WriteLine("Aradığınız kriterlere uygun kart board'da bulunamadı.\n");
+    }
+}
---
+++ patikaodevleri/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    class Kisi
+    {
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+        public string TelefonNumarasi { get; set; }
+
+    }
+    static List<Kisi> telefonRehberi = new List<Kisi>();
+
+    static void Main(string[] args)
+    {
+
+        int secim;
+
+        do
+        {
+
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :) ");
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("(1) Yeni Numara Kaydetmek");
+            Console.WriteLine("(2) Varolan Numarayı Silmek");
+            Console.WriteLine("(3) Varolan Numarayı Güncelleme");
+            Console.WriteLine("(4) Rehberi Listelemek");
+            Console.WriteLine("(5) Rehberde Arama Yapmak");
+            Console.WriteLine("(0) Çıkış");
+
+            secim = int.Parse(Console.ReadLine());
+
+            switch (secim)
+            {
+                    case 1:
+                    YeniNumaraKaydet();
+                    break;
+
+                    case 2:
+                    VarOlanNumaraSil();
+                    break;
+
+                    case 3:
+                    VarolanNumaraGuncelle();
+                    break;
+
+                    case 4:
+                    RehberiListele();
+                    break;
+
+                    case 5:
+                    RehberdeAramaYap();
+                    break;
+
+                case 0:
+                    Console.WriteLine("cikis yapiliyorrr.. ");
+                    break;
+
+                    default:
+                    Console.WriteLine("lutfen gecerli bir secim yapiniz");
+                    break ;
+
+
+
+            }
+
+
+
+
+
+        } while (secim != 0);
+
+
+
+
+
+
+
+    }
+
+    static void YeniNumaraKaydet()
+    {
+        Kisi yeniKisi = new Kisi();
+
+        Console.WriteLine("lutfen isim giriniz: ");
+        yeniKisi.Isim = Console.ReadLine();
+
+        Console.WriteLine("lutfen soyisim giriniz: ");
+        yeniKisi.Soyisim = Console.ReadLine();
+
+        Console.WriteLine("lutfen numara giriniz:  ");
+        yeniKisi.TelefonNumarasi = Console.ReadLine();
+
+        telefonRehberi.Add(yeniKisi);
+        Console.WriteLine("yeni kisi eklendi");
+
+    }
+
+
+    static void VarOlanNumaraSil()
+    {
+
+        Console.Write("Lütfen silmek istediğiniz kişinin ismini ya da soyismini giriniz: ");
+        
+        string isimOrSoyisim =Console.ReadLine();
+ 
+        Kisi kisi =telefonRehberi.FirstOrDefault(k => k.Isim == isimOrSoyisim);
+        Kisi kisi1 =telefonRehberi.FirstOrDefault(k => k.Soyisim == isimOrSoyisim);
+
+        if (kisi1 != null)
+        {
+            telefonRehberi.Remove(kisi1);
+        }
+
+        else if (kisi != null)
+        {
+            telefonRehberi.Remove(kisi);
+        }
+
+        else
+        {
+            Console.WriteLine("kayit bulunamadi");
+        }
+
+
+
+    }
+    static void VarolanNumaraGuncelle()
+    {
+        Console.Write("Lütfen güncellemek istediğiniz kişinin ismini giriniz: ");
+        string isim = Console.ReadLine();
+
+        Kisi kisi = telefonRehberi.FirstOrDefault(k => k.Isim == isim);
+        if (kisi != null)
+        {
+            Console.Write("Yeni telefon numarasını giriniz: ");
+            kisi.TelefonNumarasi = Console.ReadLine();
+            Console.WriteLine("Numara başarıyla güncellendi.\n");
+        }
+        else
+        {
+            Console.WriteLine("Kayıt bulunamadı.\n");
+        }
+    }
+    static void RehberiListele()
+    {
+        if (telefonRehberi.Count == 0)
+        {
+            Console.WriteLine("Rehber boş.\n");
+        }
+        else
+        {
+            Console.WriteLine("Telefon Rehberi:");
+            foreach (var kisi in telefonRehberi)
+            {
+                Console.WriteLine($"İsim: {kisi.Isim}, Soyisim: {kisi.Soyisim}, Telefon Numarası: {kisi.TelefonNumarasi}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void RehberdeAramaYap()
+    {
+        Console.Write("Lütfen aramak istediğiniz ismi giriniz: ");
+        string isim = Console.ReadLine();
+
+        Kisi kisi = telefonRehberi.FirstOrDefault(k => k.Isim == isim);
+        if (kisi != null)
+        {
+            Console.WriteLine($"İsim: {kisi.Isim}, Soyisim: {kisi.Soyisim}, Telefon Numarası: {kisi.TelefonNumarasi}\n");
+        }
+        else
+        {
+            Console.WriteLine("Kayıt bulunamadı.\n");
+        }
+    }
+
+
+
+}
